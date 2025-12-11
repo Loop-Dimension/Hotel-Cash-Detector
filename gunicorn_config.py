fef8c211ce3bad93d982951cb93810a61815fa3e
Abdelrahman Abdelnasser Gamal <55161963+abdelrahman18036@users.noreply.github.com>
2025-12-11 10:26:23 +0200

    update

---
+++ gunicorn_config.py
@@ -0,0 +1,22 @@
+import multiprocessing
+
+bind = "127.0.0.1:8000"
+workers = 1
+worker_class = "gevent"
+worker_connections = 1000
+timeout = 120
+keepalive = 5
+
+# Logging
+accesslog = "/var/log/gunicorn/access.log"
+errorlog = "/var/log/gunicorn/error.log"
+loglevel = "info"
+
+# Process naming
+proc_name = "hotel-cctv"
+
+# Server mechanics
+daemon = False
+pidfile = "/home/ubuntu/Hotel-Cash-Detector/gunicorn.pid"
+user = "ubuntu"
+group = "ubuntu"
